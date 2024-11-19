70227461a18687e5af53fe9abd9ad445df28854e
arikaleeswaran <g.arikaleeswaran7@gmail.com>
2024-11-19 13:00:35 +0530

    Added Spiral Matrix Program

---
+++ Program/Code/SpiralMatrix.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    // Function to return the spiral order of a matrix
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n - 1;
+        List<Integer> ans = new ArrayList<>();
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrix sol = new SpiralMatrix();
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        List<Integer> result = sol.spiralOrder(matrix);
+
+
+        System.out.println("Spiral order of the matrix: " + result);
+    }
+}
